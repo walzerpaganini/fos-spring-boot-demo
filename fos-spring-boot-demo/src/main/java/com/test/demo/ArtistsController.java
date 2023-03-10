f654db5d39c67dd5939801adea0b1c29dde113e5
Walter <Walter@LAPTOP-M51MOGA5>
2023-03-10 23:59:46 +0100

    Lezione del 06/03/2023

--- fos-spring-boot-demo/src/main/java/com/test/demo/ArtistsController.java
+++ fos-spring-boot-demo/src/main/java/com/test/demo/ArtistsController.java
@@ -1,17 +1,20 @@
 package com.test.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("artists")
 public class ArtistsController {
-	
 	/**
 	 * L'annotazione @RequestParam permette di estrapolare parametri dalla query string della richiesta.
 	 * Se non indichiamo un nome esplicito, Spring mapperà i parametri basandosi sui nomi dei parametri del metodo.
@@ -23,7 +26,7 @@ public class ArtistsController {
 	 * Le annotazioni @RequestHeader e @CookieValue si usano in modo simile, ma leggono (ovviamente) dagli header e dai cookie della richiesta.
 	 */
 	
-	@GetMapping // 
+	@GetMapping
 	private String getAllArtists(@RequestParam(name="first-name", required=false) String name) {		
 		return "Stai cercando artisti che si chiamano " + name;
 	}
@@ -42,24 +45,56 @@ public class ArtistsController {
 	/**
 	 * Usando l'annotazione @PathVariable, possiamo estrapolare il valore di un parametro dal path.
 	 * In questo caso, l'endpoint risponde a chiamate GET sui path "/artists/1", "/artists/2", e così via.
+	 *
+	 * Per questo metodo utilizziamo come tipo di ritorno la classe generica ResponseEntity: notate che
+	 * possiamo usarla per restituire diversi tipi di body in caso di successo o insuccesso della richiesta,
+	 * e anche per impostare esplicitamente il codice di stato HTTP da inviare al client.
 	 * 
-	 * Notate che restituiamo un oggetto di una nostra classe: Spring lo serializzerà in formato JSON per costruire il body della risposta.
+	 * Notate che in caso di successo restituiamo un oggetto di una nostra classe: Spring lo serializzerà in
+	 * formato JSON per costruire il body della risposta. Questo meccanismo funziona anche restituendo direttamente
+	 * l'oggetto in questione, senza incapsularlo in un oggetto di classe ResponseEntity.
 	 * (Vedi ArtistData.java)
 	 */
 	
 	@GetMapping("{id}")
-	private ArtistData getArtistDetails(@PathVariable("id") int artistId) {
-		return new ArtistData(artistId, "Stevie", "Wonder");
+	private ResponseEntity<?> getArtistDetails(@PathVariable("id") int artistId) throws ArtistNotFoundException {
+		if(artistId == 0) {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND)
+					.body("Artista con ID " + artistId + " non trovato.");
+		}
+		
+		ArtistData ad = new ArtistData(artistId, "Stevie", "Wonder");
+		return ResponseEntity.ok(ad);
 	}
 	
 	/**
 	 * Possiamo anche estrapolare più parametri da un singolo path, distinguendoli con nomi diversi.
-	 * Al solito, se non indichiamo un nome esplicito, Spring cercherà di mappare i valori usando i nomi dei parametri del metodo.
+	 * Al solito, se non indichiamo un nome esplicito, Spring cercherà di mappare i valori usando i 
+	 * nomi dei parametri del metodo.
 	 */
 	
 	@GetMapping("{artistId}/albums/{albumId}")
-	private String getDetailOfAlbum(@PathVariable Long artistId, @PathVariable Long albumId) {
+	private String getDetailOfAlbum(@PathVariable Integer artistId, @PathVariable Long albumId) throws ArtistNotFoundException {
+		if(artistId == 0) {
+			// Sollevando questa eccezione, la gestione della richiesta passerà a un metodo annotato
+			// con @ExceptionHandler (se il controller ne contiene uno adatto).
+			
+			throw new ArtistNotFoundException(artistId);
+		}
+		
 		return "Dettagli dell'album " + albumId + " dell'artista con ID " + artistId;
 	}
 
+	/**
+	 * Annotando un metodo con @ExceptionHandler, esso viene invocato ogni volta che un altro metodo del
+	 * controller solleva una delle eccezioni indicate. Si può usare @ResponseStatus per stabilire il codice 
+	 * di stato HTTP, e allo stesso tempo restituire un oggetto a nostro piacimento come body della risposta.
+	 */
+	
+	@ExceptionHandler({ ArtistNotFoundException.class })
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private String handleArtistNotFound(ArtistNotFoundException e) {
+		return "Artista con ID " + e.getArtistId() + " non trovato.";
+	}
 }
--- fos-spring-boot-demo/src/main/java/com/test/demo/DemoRestController.java
+++ fos-spring-boot-demo/src/main/java/com/test/demo/DemoRestController.java
@@ -1,9 +1,15 @@
 package com.test.demo;
 
+import java.io.IOException;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  *  L'annotazione @RestController fa sì che questa classe venga usata da Spring per intercettare
@@ -37,5 +43,26 @@ public class DemoRestController {
 	@GetMapping("sub") // Risponde a chiamate GET su "/api-rest/sub"
 	private String handleMoreSpecificGetRequests() {
 		return "Received GET on /api-rest/sub";
+	}
+	
+	/**
+	 * Se aggiungiamo un parametro di tipo HttpServletRequest o HttpServletResponse,
+	 * Spring che inietterà un oggetto corrispondente che rappresenta rispettivamente
+	 * la richiesta HTTP ricevuta e la risposta HTTP che verrà inviata al client.
+	 * Usando i metodi delle loro interfacce, possiamo conoscere qualsiasi dettaglio
+	 * tecnico della richiesta e modificare qualsiasi dettaglio tecnico della risposta.
+	 * 
+	 * Notate che in questo caso, volendo, potremmo perfino lasciare il metodo con tipo
+	 * di ritorno void e scrivere direttamente i byte nello stream della risposta.
+	 */
+	
+	@GetMapping("servlet-stuff")
+	private void httpRequestAndResponse(HttpServletRequest req, HttpServletResponse res) throws IOException {
+		System.out.println(req.getCharacterEncoding());
+		
+		res.setStatus(409);
+		res.addCookie(new Cookie("mycookie", "value"));
+		
+		res.getOutputStream().write("Prova".getBytes());
 	}
 }
